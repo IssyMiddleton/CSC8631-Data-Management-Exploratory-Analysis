ff4d507ae0a22b9f18b08e4de9453cc1181e2ff0
IssyMiddleton <issy.middleton@yahoo.com>
2020-12-01 12:55:50 +0000

    Updated colour scheme for graphs

--- Exploratory_Analysis/reports/README.md
+++ Exploratory_Analysis/reports/README.md
@@ -1,1 +1,3 @@
-Here you can store any output reports, such as HTML or LaTeX versions of tables, that you produce. Sweave or brew documents should also go in the `reports` directory.
+Within the reports folder are stored the source RMarkdown documents (ending in .Rmd) and the output documents entitled Report.pdf and Overview-Presentation in PowerPoint format. These can be reproduced by using Knit on the RMarkdown source documents. The Overview Presentation uses the presentation template (.potx) for formatting.
+
+By embedding sections of code within the reports they will automatically update if there are changes.
--- Exploratory_Analysis/src/01 - Analysis.R
+++ Exploratory_Analysis/src/01 - Analysis.R
@@ -1,136 +1,188 @@
 library('ProjectTemplate')
 load.project()
 
-dev.new(width=4, height=3)
+#This analysis file generates a series of graphs which are saved in the graphs
+#folder. The Overview charts are first, then Gender, Age Range, Employment Area,
+#Employment Status and Highest Education
 
+if (!require("RColorBrewer")) {
+  install.packages("RColorBrewer")
+  library(RColorBrewer)
+}
+
+#Overview Charts    
+Shared_Info = ggplot(enrol, aes(x=Shared_Info))+
+  theme_bw() +
+  geom_bar(colour ="steelblue", fill = "steelblue") +
+  geom_text(aes(label=..count..),stat="count",position=position_stack(0.5)) +
+  scale_x_continuous(breaks = c(0:5),labels = factor(0:5)) +
+  labs(y = "Survey Responses", x = "No. of details shared", title = "Amount of information shared by students")
+ggsave(file.path('graphs', 'Shared_Info.pdf'), width = 5, height = 3)
+
+Annual_Survey_Response = ggplot(enrol, aes(x=year, fill = survey))+
+  theme_bw() +
+  geom_bar(stat = "count") +
+  geom_text(aes(label=..count..),stat="count",position=position_stack(0.5)) +
+  scale_fill_brewer(palette="BrBG") +
+  labs(y = "Survey Responses", x = "Year", fill ="Survey No.", title = "Survey responses by course and year")
+ggsave(file.path('graphs', 'Annual_Survey_Response.pdf'), width = 5, height = 3)
+
+Survey_Response = ggplot(enrol, aes(x=survey))+
+  theme_bw() +
+  geom_bar(colour ="steelblue", fill = "steelblue")  +
+  geom_text(aes(label=..count..),stat="count",position=position_stack(0.5)) +
+  labs(y = "Survey Responses", x = "Survey", title = "Enrolment surveys by course")
+ggsave(file.path('graphs', 'Survey_Response_By_Course.pdf'), width = 5, height = 3)
+
+Year_Response = ggplot(enrol, aes(x=year))+
+  theme_bw() +
+  geom_bar(colour ="steelblue", fill = "steelblue")  +
+  geom_text(aes(label=..count..),stat="count",position=position_stack(0.5)) +
+  labs(y = "Survey Responses", x = "Year", title = "Enrolment surveys by year")
+ggsave(file.path('graphs', 'Survey_Response_By_Year.pdf'), width = 5, height = 3)
+
+
+# Shared Gender Charts
 Shared_Gender = ggplot(shared_g, aes(x=gender, fill = employment_status))+
   theme_bw() +
   geom_bar() +
+  scale_fill_brewer(palette="BrBG") +
   labs(y = "Count of Survey Responses", x = "Gender", fill = "Employment Status",
-       title = "Students that provided gender", subtitle = " ")
-  ggsave(file.path('graphs', 'Shared_Gender.pdf'))
+       title = "Students that provided gender")
+  ggsave(file.path('graphs', 'Shared_Gender.pdf'), width = 5, height = 3)
 
 Shared_Gender_by_Age = ggplot(shared_g, aes(x=gender, fill = age_range))+
   theme_bw() +
   geom_bar() +
+  scale_fill_brewer(palette="BrBG") +
   labs(y = "Survey Response", x = "Gender", fill = "Age Range",
-       title = "Students that provided gender and age range", subtitle = " ")
-  ggsave(file.path('graphs', 'Shared_Gender_by_Age.pdf'))
+       title = "Students that provided gender and age range")
+  ggsave(file.path('graphs', 'Shared_Gender_by_Age.pdf'), width = 5, height = 3)
 
 Shared_Gender_by_Emp_by_Survey = ggplot(shared_g, aes(x=gender, fill = employment_status))+
   theme_bw() +
   geom_bar() +
   facet_wrap(~ survey) +
+  scale_fill_brewer(palette="BrBG") +
   labs(y = "Survey Response", x = "Gender", fill = "Employment Status",
-       title = "Students that provided gender and employment status", subtitle = " ")
-  ggsave(file.path('graphs', 'Shared_Gender_by_Emp_by_Survey.pdf'))
+       title = "Students that provided gender and employment status")
+  ggsave(file.path('graphs', 'Shared_Gender_by_Emp_by_Survey.pdf'), width = 5, height = 3)
 
 Shared_Gender_by_Age_by_Survey = ggplot(shared_g, aes(x=gender, fill = age_range))+
   theme_bw() +
   geom_bar() +
   facet_wrap(~ survey) +
+  scale_fill_brewer(palette="BrBG") +
   labs(y = "Survey Response", x = "Gender", fill = "Age Range",
-       title = "Students that provided gender and age", subtitle = " ")
-  ggsave(file.path('graphs', 'Shared_Gender_by_Age_by_Survey.pdf'))
+       title = "Students that provided gender and age")
+  ggsave(file.path('graphs', 'Shared_Gender_by_Age_by_Survey.pdf'), width = 5, height = 3)
   
 Shared_MF_by_Age_by_Survey = ggplot(shared_mf, aes(x=gender, fill = age_range))+
     theme_bw() +
     geom_bar() +
     facet_wrap(~ survey) +
-    labs(y = "Survey Response", x = "Gender", fill = "Age Range",
-         title = "Male or female students that provided age range", subtitle = " ")
-  ggsave(file.path('graphs', 'Male_Female_by_Age_by_Survey.pdf'))
+  scale_fill_brewer(palette="BrBG") +
+  labs(y = "Survey Response", x = "Gender", fill = "Age Range",
+         title = "Male or female students that provided age range")
+  ggsave(file.path('graphs', 'Male_Female_by_Age_by_Survey.pdf'), width = 5, height = 3)
   
 Shared_MF_by_Age = ggplot(shared_mf, aes(x=gender, fill = age_range))+
     theme_bw() +
     geom_bar() +
-    labs(y = "Survey Response", x = "Gender", fill = "Age Range",
-         title = "Male or female students that provided age range", subtitle = " ")
-  ggsave(file.path('graphs', 'Male_Female_by_Age.pdf'))
-  
+  scale_fill_brewer(palette="BrBG") +
+  labs(y = "Survey Response", x = "Gender", fill = "Age Range",
+         title = "Male or female students that provided age range")
+  ggsave(file.path('graphs', 'Male_Female_by_Age.pdf'), width = 5, height = 3)
 
+# Shared Age Charts 
 Shared_Age_and_Gender_by_Year = ggplot(shared_mf, aes(x=gender, fill = age_range))+
     theme_bw() +
     geom_bar() +
     facet_wrap(~ year) +
-    labs(y = "Survey Response", x = "Gender", fill = "Age Range",
-         title = "Male or female students that provided age range", subtitle = " ")
-  ggsave(file.path('graphs', 'Male_Female_by_Age_by_Year.pdf'))
+  scale_fill_brewer(palette="BrBG") +
+  labs(y = "Survey Response", x = "Gender", fill = "Age Range",
+         title = "Male or female students that provided age range")
+  ggsave(file.path('graphs', 'Male_Female_by_Age_by_Year.pdf'), width = 5, height = 3)
 
-Annual_Survey_Response = ggplot(enrol, aes(x=year, fill = survey))+
+Age_Range_Total = ggplot(shared_a, aes(x=survey, fill = age_range))+
     theme_bw() +
-    geom_bar(stat = "count") +
-  geom_text(aes(label=..count..),stat="count",position=position_stack(0.5)) +
-    labs(y = "Survey Responses", x = "Year", fill ="Survey No.", title = "Survey responses by course and year", subtitle = " ")
-  ggsave(file.path('graphs', 'Annual_Survey_Response.png'))
+    geom_bar()+
+    scale_x_continuous(breaks = c(1:7), 
+                       labels = factor(1:7)) +
+  scale_fill_brewer(palette="BrBG") +
+  labs(y = "Survey Response", x = "Survey", fill = "Age Range", 
+         title = "Students that provided age range")
+  ggsave(file.path('graphs', 'Survey_Responses_on_Age_Range.pdf'), width = 5, height = 3)
   
-  
-Survey_Response = ggplot(enrol, aes(x=survey))+
+Age_Range_by_Year = ggplot(shared_a, aes(x=year, fill = age_range))+
     theme_bw() +
-    geom_bar(colour ="steelblue", fill = "steelblue")  +
-  geom_text(aes(label=..count..),stat="count",position=position_stack(0.5)) +
-    labs(y = "Survey Responses", x = "Survey", title = "Enrolment surveys by course", subtitle = " ")
-  ggsave(file.path('graphs', 'Survey_Response_By_Course.png'), width = 2, height = 2)
+    geom_bar() +
+  scale_fill_brewer(palette="BrBG") +
+  labs(y = "Survey Response", x = "Year", fill = "Age Range", 
+         title = "Students that provided age range")
+  ggsave(file.path('graphs', 'Survey_Responses_on_Age_Range_by_Year.pdf'), width = 5, height = 3)
   
-Year_Response = ggplot(enrol, aes(x=year))+
-    theme_bw() +
-    geom_bar(colour ="steelblue", fill = "steelblue")  +
-    geom_text(aes(label=..count..),stat="count",position=position_stack(0.5)) +
-    labs(y = "Survey Responses", x = "Year", title = "Enrolment surveys by year", subtitle = " ")
-  ggsave(file.path('graphs', 'Survey_Response_By_Year.png'), width = 2, height = 2)
-  
-  
+# Employment Area charts  
 Employment_Area_Total = ggplot(shared_ea, aes(x=survey, fill = employment_area))+
     theme_bw() +
   geom_bar()+
   scale_x_continuous(breaks = c(1:7), 
                      labels = factor(1:7)) +
   theme(legend.text = element_text(color = "black", size = 7)) +
+  scale_fill_brewer(palette="BrBG") +
   labs(y = "Survey Response", x = "Survey", fill = "Employment Area", 
-       title = "Students that provided employment area", subtitle = " ")
-  ggsave(file.path('graphs', 'Survey_Responses_on_Employment_Area.pdf'))
+       title = "Students that provided employment area")
+  ggsave(file.path('graphs', 'Survey_Responses_on_Employment_Area.pdf'), width = 5, height = 3)
 
 Employment_Area_by_Year = ggplot(shared_ea, aes(x=year, fill = employment_area))+
   theme_bw() +
   geom_bar() +
-  scale_x_continuous(breaks = c(1:7), 
-                     labels = factor(1:7)) +
   theme(legend.text = element_text(color = "black", size = 7)) +
+  scale_fill_brewer(palette="BrBG") +
   labs(y = "Survey Response", x = "Year", fill = "Employment Area", 
-       title = "Students that provided employment area", subtitle = " ")
-  ggsave(file.path('graphs', 'Survey_Responses_on_Employment_Area_by_Year.pdf'))
+       title = "Students that provided employment area")
+  ggsave(file.path('graphs', 'Survey_Responses_on_Employment_Area_by_Year.pdf'), width = 5, height = 3)
 
+#Employment Status Charts
 Employment_Status_Total = ggplot(shared_es, aes(x=survey, fill = employment_status))+
   theme_bw() +
   geom_bar() +
   scale_x_continuous(breaks = c(1:7), 
                      labels = factor(1:7)) +
+  scale_fill_brewer(palette="BrBG") +
   labs(y = "Survey Response", x = "Survey", fill = "Employment Status", 
-       title = "Students that provided employment status", subtitle = " ")
-  ggsave(file.path('graphs', 'Survey_Responses_on_Employment_Status.pdf'))
+       title = "Students that provided employment status")
+  ggsave(file.path('graphs', 'Survey_Responses_on_Employment_Status.pdf'), width = 5, height = 3)
                 
 Employment_Status_by_Year = ggplot(shared_es, aes(x=year, fill = employment_status))+
   theme_bw() +
   geom_bar() +
+  scale_fill_brewer(palette="BrBG") +
   labs(y = "Survey Response", x = "Year", fill = "Employment Status", 
-       title = "Students that provided information on employment status", subtitle = " ")
-  ggsave(file.path('graphs', 'Survey_Responses_on_Employment_Status_by_Year.pdf'))
-       
-Not_Provided = ggplot(notshared_g, aes(x=survey, fill = age_range))+
-  theme_bw() +
-  geom_bar() +
-  labs(y = "Survey Response", x = "Intake/Survey", fill = "Age Range", 
-       title = "Students that didn't provided information on gender", subtitle = " ")
+       title = "Students that provided employment status")
+  ggsave(file.path('graphs', 'Survey_Responses_on_Employment_Status_by_Year.pdf'), width = 5, height = 3)
+ 
+#Highest Education Charts   
+Highest_Ed_Total = ggplot(shared_h, aes(x=survey, fill = highest_education_level))+
+    theme_bw() +
+    geom_bar() +
+    scale_x_continuous(breaks = c(1:7), 
+                       labels = factor(1:7)) +
+  scale_fill_brewer(palette="BrBG") +
+  labs(y = "Survey Response", x = "Survey", fill = "Employment Status", 
+         title = "Students that provided highest education")
+  ggsave(file.path('graphs', 'Survey_Responses_on_Highest_Education_Status.pdf'), width = 5, height = 3)
+  
+Highest_Ed_by_Year = ggplot(shared_h, aes(x=year, fill = highest_education_level))+
+    theme_bw() +
+    geom_bar() +
+  scale_fill_brewer(palette="BrBG") +
+    labs(y = "Survey Response", x = "Year", fill = "Highest Education", 
+         title = "Students that provided highest education")
+  ggsave(file.path('graphs', 'Survey_Responses_on_Highest_Education_by_Year.pdf'), width = 5, height = 3)
 
-Percent = plot(prop.table(table(enrol$gender)))
 
-Shared_Info = ggplot(enrol, aes(x=Shared_Info))+
-  theme_bw() +
-  geom_bar(colour ="steelblue", fill = "steelblue") +
-  geom_text(aes(label=..count..),stat="count",position=position_stack(0.5)) +
-  scale_x_continuous(breaks = c(0:5),labels = factor(0:5)) +
-  labs(y = "Survey Responses", x = "No. of details shared", title = "Amount of information shared by students", subtitle = " ")
-ggsave(file.path('graphs', 'Shared_Info.png'), width = 3, height = 3)
 
+#Other calculations
 course_numbers = summary(enrol$survey)
 
